5146a41be8bf4543b20afea56e8279edc293836b
Spoffy <spoffeh@gmail.com>
2015-10-11 15:50:14 +0100

    Modified data structure sent, replaced send_message with send, encode and decode

--- learntocode/analytics.py
+++ learntocode/analytics.py
@@ -1,7 +1,9 @@
 import json
 from . import network
 
-class ErrorDebugInfo(network.Message):
+class ErrorDebugInfo():
+    MESSAGE_TYPE = "DEBUG_INFO"
+
     def __init__(self, trace, file_path, source):
         self.trace = trace
         self.file_path = file_path
@@ -10,12 +12,15 @@ class ErrorDebugInfo(network.Message):
     def __str__(self):
         return "\n".join([str(self.file_path), str(self.source), str(self.trace)])
 
-    def to_json(self):
-        return json.dumps({
+    def serialise(self):
+        return {
             "trace": self.trace,
             "file_path": self.file_path,
             "source": self.source
-        })
+        }
+
+    def deserialise(self):
+        pass
 
 def debug_info_handler(debug_info, exception_class):
-    network.send_message(debug_info)
+    network.send(debug_info)
--- learntocode/network.py
+++ learntocode/network.py
@@ -7,19 +7,20 @@ import uuid
 SERVER_ADDRESS = "http://127.0.0.1:8080/analytics"
 USER_ID = str(uuid.getnode())
 
-class Message:
-    def to_dict():
-        """This should be overridden by inheriting classes"""
-        return dict()
+def encode(message):
+    return json.dumps({
+        "user_id": USER_ID,
+        "time": time.time(),
+        "message_type": message.MESSAGE_TYPE,
+        "message": message.serialise()
+    })
 
-def send(data):
-    packet = {
-            "user_id": USER_ID,
-            "time": time.time(),
-            "message": data
-    }
-    requests.post(SERVER_ADDRESS, data=json.dumps(packet), 
+def decode(raw_json):
+    pass
+
+def send(message):
+    requests.post(SERVER_ADDRESS, data=encode(message),
             headers={"content-type": "application/json"})
 
-def send_message(message):
-    send(message.to_dict())
+
+
