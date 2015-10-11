41561f58d580adedeb4f49949ffd12858aea8b1d
Spoffy <spoffeh@gmail.com>
2015-10-11 16:02:35 +0100

    Added framework for handling messages based on type

--- learntocode/network.py
+++ learntocode/network.py
@@ -1,11 +1,14 @@
 import json
 import requests
+import sys
 import time
 import uuid
 
 #Temporary until configurations are implemented
 SERVER_ADDRESS = "http://127.0.0.1:8080/analytics"
 USER_ID = str(uuid.getnode())
+
+_message_handlers = dict()
 
 def encode(message):
     return json.dumps({
@@ -15,12 +18,17 @@ def encode(message):
         "message": message.serialise()
     })
 
-def decode(raw_json):
-    pass
+def on_message_received(raw_json):
+    try:
+        packet = json.loads(raw_json)
+        handler = _message_handlers[packet["message_type"]]
+        handler(packet["message"])
+    except KeyError:
+        print("Malformed JSON, unable to handle", file=sys.stderr)
 
 def send(message):
     requests.post(SERVER_ADDRESS, data=encode(message),
             headers={"content-type": "application/json"})
 
-
-
+def add_message_handler(type, handler):
+    _message_handles[type] = handler
