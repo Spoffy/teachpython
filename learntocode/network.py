82fc0c46dc2dfb868bd7eb2025aa61ca033f8745
Spoffy <spoffeh@gmail.com>
2015-10-11 15:33:24 +0100

    Added uniform structure for sending messages

--- learntocode/network.py
+++ learntocode/network.py
@@ -1,17 +1,25 @@
-import requests
 import json
+import requests
+import time
+import uuid
 
 #Temporary until configurations are implemented
 SERVER_ADDRESS = "http://127.0.0.1:8080/analytics"
+USER_ID = str(uuid.getnode())
 
 class Message:
-    _default_message = dict()
-
-    def to_json():
-        return json.dumps(_default_message)
+    def to_dict():
+        """This should be overridden by inheriting classes"""
+        return dict()
 
 def send(data):
-    requests.post(SERVER_ADDRESS, data=data, headers={"content-type": "application/json"})
+    packet = {
+            "user_id": USER_ID,
+            "time": time.time(),
+            "message": data
+    }
+    requests.post(SERVER_ADDRESS, data=json.dumps(packet), 
+            headers={"content-type": "application/json"})
 
-def send_message(debug_info):
-    send(debug_info.to_json())
+def send_message(message):
+    send(message.to_dict())
