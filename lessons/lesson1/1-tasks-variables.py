2de4f91736d1acf0ed0bebd0f66638840d98a8ab
Spoffy <spoffeh@gmail.com>
2015-10-28 16:11:07 +0000

    [Lesson 1] Moving files to a sensible structure

---
+++ lessons/lesson1/1-tasks-variables.py
@@ -0,0 +1,11 @@
+"""
+Tasks
+##########
+
+Here are a few tasks to attempt regarding variables!
+Each task has instructions above it.
+
+In order to attempt a task, uncomment the code that goes with it and follow the
+instructions!
+"""
+
