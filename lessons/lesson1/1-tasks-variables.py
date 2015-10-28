0916a583572cdafe2f887db064a6a130b65961ca
Spoffy <spoffeh@gmail.com>
2015-10-28 16:19:00 +0000

    [Lesson 1] Tasks written for variables

--- lessons/lesson1/1-tasks-variables.py
+++ lessons/lesson1/1-tasks-variables.py
@@ -9,3 +9,64 @@ In order to attempt a task, uncomment the code that goes with it and follow the
 instructions!
 """
 
+#Tasks begin below!
+
+"""
+Task 01
+##########
+
+Can you write a program that writes 'This is a simple program!' to the console?
+Reminder: The 'print' statement can be used to provide textual output from a program to the console.
+
+"""
+
+#Write your code below:
+
+
+"""
+Task 02
+##########
+
+Can you finish the program below so that:
+  the variable mystring is assigned to the value of "Python!"
+  the variable pi is assigned to the value of 3.14
+  the variable seven is assigned to the value of 7
+
+"""
+
+#Uncomment and complete the block below
+"""
+mystring =
+print(mystring)
+
+pi
+print(pi)
+
+seven
+print(seven)
+"""
+
+"""
+Task 03
+
+Can you finish the program below so that:
+  the variable mystring is assigned to the value of "Python" by string concatenation
+  the variable two is assigned to the value of 2 by addition
+  the variable fourteen is assigned to the value of 14 by multiplication
+  the variable nine is assigned to the value of 9 by using exponents (powers).
+"""
+
+#Uncomment and complete the block below
+"""
+mystring = "Pyt"  
+print(mystring)
+
+two = 1  
+print(two) 
+
+fourteen = 7 
+print(fourteen)
+
+nine = 3 
+print(nine)
+"""
