e580f32476be8be12a72c041a9027c56827e280f
Spoffy <spoffeh@gmail.com>
2015-10-29 18:35:25 +0000

    [Lesson 1] Added tasks for lists

---
+++ lessons/lesson1/2-tasks-lists.py
@@ -0,0 +1,23 @@
+"""
+Tasks
+##########
+
+Here are a few tasks to attempt regarding variables!
+Each task has instructions above it.
+
+In order to attempt a task, uncomment the code that goes with it and follow the
+instructions!
+"""
+
+"""
+Task 01
+##########
+
+Finish the program to print out the third element, followed by the 
+multiplication of the second and fourth elements, then followed by the 
+first element of the list mylist.
+"""
+
+mylist = ["pies.", 3, "I like to eat ", 11]
+
+#Write your code below
