26e8be46b05676e9d665b4201a86970ac962caa5
Spoffy <spoffeh@gmail.com>
2015-10-30 18:10:41 +0000

    [Lesson 1] Added Further tasks

---
+++ lessons/lesson1/5-tasks-further.py
@@ -0,0 +1,44 @@
+"""
+Tasks
+##########  
+
+Here are a some further, more advanced tasks you might want to try!
+Warning: We may not have covered all of the material for these!
+"""
+
+"""
+Task 01
+##########   
+
+ Write a program that will print out the contents of a multiplication table from 1x1 to 12x12.
+  ie. The multiplication table from 1x1 to 3x3 is as follows:
+ 	1	2	3
+ 	2	4	6
+ 	3	6	9
+
+ Hint: You'll probably want to use two for loops for each number.
+
+ Reminder: To print something without putting a newline on the end, you can use print(..., end="\t").
+ 			You might also want to use tabs so double/triple digit numbers are formatted in a nice table.
+
+ Bonus: See if you can figure out how to print the column headers.
+"""
+
+#Write your code below: 
+
+"""
+Task 02
+##########   
+
+Use a for loop and the knowledge you know about modulo to create a FizzBuzz program.
+In the program, the following happens:
+	Counting up from 1 to 100:
+		If the number is divisible by 3, print Fizz.
+		If the number is divisible by 5, print Buzz.
+		If the number is divisible by 3 and 5, print FizzBuzz.
+		Otherwise, just print the number.
+
+Reminder: You can look back at the previous tasks to see how to print, use a for loop and the modulo operator.
+"""
+
+#Write your code below:
